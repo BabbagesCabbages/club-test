f158905fc610c91ec24f2da7385c2022a08f4a7b
Karson Madden <krm@karsonm-2.local>
2016-09-21 12:53:27 -0700

    closed more issues

--- src/javascript/app/models/SideNavModel.js
+++ src/javascript/app/models/SideNavModel.js
@@ -5,17 +5,17 @@ module.exports = Backbone.Model.extend({
     defaults: {
         onload: true,
         intro1: {
-          title: 'Introduction No',
+          title: 'Welcome!',
           slide: 1,
           text: 'What is this all about?',
         },
         intro2: {
-          title: 'Introduction No',
+          title: 'Introduction',
           slide: 2,
-          text: 'Introduction to precision medicine.',
+          text: 'Introduction to precision medicine',
         },
         intro3: {
-          title: 'Introduction No',
+          title: 'Introduction',
           slide: 3,
           text: 'Precision medicine: What is adjuvant treatment?',
         },
