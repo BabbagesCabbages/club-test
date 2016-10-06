30f5991ce0175a03506730a74cef61f914a7bafc
Karson Madden <krm@karsonm-2.local>
2016-10-06 06:08:08 -0700

    fixed patient2view2 and patient3view2 mammorgram interactives. also added reattached slide 44

--- src/javascript/app/views/patient2/Patient2View2.js
+++ src/javascript/app/views/patient2/Patient2View2.js
@@ -9,7 +9,9 @@ module.exports = BaseView.extend({
     ui: {},
     events: {
         'click .continue': 'scrollTop',
-        "click .correct": "handleClick",
+        'click .cancer': 'handleClick',
+        'click #next-page':'nextPage',
+        'click image': 'handleClick2'
     },
 
     initialize: function() {
@@ -27,19 +29,30 @@ module.exports = BaseView.extend({
     onBeforeDestroy: function() {},
 
     handleClick: function() {
-      $('path.circle').fadeIn('fast');
+      $('#modalIncorrect').modal('hide');
+      $('.circle').css('display','block');
+      $('.circle2').css('display','block');
+      $('.st0').css('display','block');
+      $('#modalCorrect').modal('show');
+      $('.close-modal').click(function(e) {
+        if (e.target) {
+          $('#modalIncorrect').modal('hide');
+        }
+      });
+    },
+    handleClick2: function() {
+      $('#modalIncorrect').modal('show');
     },
     scrollTop: function(){
-    	$('body').animate({ scrollTop: '0' }, 400);  $('body,html').animate({ scrollTop: '0' }, 400); 
+    	$('body').animate({ scrollTop: '0' }, 400);  $('body,html').animate({ scrollTop: '0' }, 400);
     },
     nextPage: function() {
-
-    },
-
-    onDestroy: function() {
       $('#myModal').modal('hide');
       $('body').removeClass('modal-open');
       $('.modal-backdrop').remove();
+    },
+
+    onDestroy: function() {
     }
 
 });
