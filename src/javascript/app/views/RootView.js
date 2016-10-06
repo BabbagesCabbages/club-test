3a20b8d6e8e9f938889293704798c39ac7c50483
Karson Madden <krm@karsonm-2.local>
2016-10-05 20:02:08 -0700

    testing jquery detect browser

--- src/javascript/app/views/RootView.js
+++ src/javascript/app/views/RootView.js
@@ -38,21 +38,11 @@ module.exports = Marionette.LayoutView.extend({
             }
             e.preventDefault(); // prevent the default action (scroll / move caret)
         });
-        var ie = (function () {
-            var undef, v = 3, div = document.createElement('div');
 
-            while (
-                div.innerHTML = '<!--[if gt IE '+(++v)+']><i></i><![endif]-->',
-                div.getElementsByTagName('i')[0]
-            );
-
-            return v > 4 ? v : undef;
-        }());
-        if ( ie <= 10 ) {
-
+        if ($.browser.msie && $.browser.version <= 10) {
               $('#modalBrowser').modal('show');
-
         }
+
     },
 
     setListeners: function() {
